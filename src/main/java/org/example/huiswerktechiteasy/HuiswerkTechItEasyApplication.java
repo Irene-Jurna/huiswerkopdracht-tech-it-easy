c5c28883725ea77ba9dd66772396816801b8a2b1
Irene <irene.jurna@gmail.com>
2025-06-01 14:53:54 +0200

    Initial commit

---
+++ src/main/java/org/example/huiswerktechiteasy/HuiswerkTechItEasyApplication.java
@@ -0,0 +1,13 @@
+package org.example.huiswerktechiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiswerkTechItEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuiswerkTechItEasyApplication.class, args);
+    }
+
+}
